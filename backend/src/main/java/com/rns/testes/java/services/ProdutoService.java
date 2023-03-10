5fbeb5ffc866035403087641bf5323d0d288b6bc
Caroline Souza <loh_jg@hotmail.com>
2023-03-09 23:43:27 -0300

    update produto

--- backend/src/main/java/com/rns/testes/java/services/ProdutoService.java
+++ backend/src/main/java/com/rns/testes/java/services/ProdutoService.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,6 +55,22 @@ public class ProdutoService {
 		Produto entity = obj.orElseThrow(()->new ResourceNotFoundException("Entidade não encontrada"));
 		 
 		return new ProdutoDTO(entity);
+	}
+	
+	@Transactional
+	public ProdutoDTO update(int id,ProdutoDTO dto) {
+		try {
+			Produto entity = repository.getOne(id);
+			entity.setNome(dto.getNome());
+			Date date = new Date();
+			entity.setDataUltAlteracao1(date);
+			entity = repository.save(entity);
+			
+			return new ProdutoDTO(entity);
+		}catch(EntityNotFoundException e) {
+			throw new  ResourceNotFoundException("Id não encontrado "+id);
+		}
+		
 	}
 
 	
