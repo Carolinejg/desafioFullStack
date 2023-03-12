23ac5eb540976fdb90832c953e615fa0462b4ade
Caroline Souza <loh_jg@hotmail.com>
2023-03-11 21:14:19 -0300

    adicionando botões

--- frontend/lib/home/home_page.dart
+++ frontend/lib/home/home_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text(''),),
-      body: Container(),
+      appBar: AppBar(
+        title: const Text('Controle de Estoque'),
+      ),
+      // ignore: sized_box_for_whitespace
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'HTTP',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'DIO',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'GET',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
