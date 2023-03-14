d638b6aa7e4e837b88aaa55061ca5549ca5d37ba
Caroline Souza <loh_jg@hotmail.com>
2023-03-14 00:37:18 -0300

    adicionando script e classe de teste

---
+++ backend/src/test/java/com/rns/testes/java/tests/repositories/ProdutoRepositoryTests.java
@@ -0,0 +1,9 @@
+package com.rns.testes.java.tests.repositories;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class ProdutoRepositoryTests {
+	
+
+}
